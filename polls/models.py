a812ca969909c06b98a01a1087562f7670f2c043
thennys <thennyswhyte@gmail.com>
2024-12-31 22:48:47 -1200

    Using render and Http404

--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=255)
     created = models.DateTimeField("Date Published")
 
+    def __str__(self):
+        return self.question_text()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice = models.CharField(max_length=255)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,27 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("Kast Your votes, fellas ")
+    latest_question_list = Question.objects.order_by("-created")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question-id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render (request, "polls/detail.html", {"question" question})
+
+def results(request, question_id):
+    response = "You are looking at %s."
+    return HttpResponse (response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse ("You are voting on question %s at %s." % question_id)
+
