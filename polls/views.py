0e2ebe1ce94983194b0c624b104e1c4d1aee6749
thennys <thennyswhyte@gmail.com>
2025-01-01 08:57:22 -1200

    templates added

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,23 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-created")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request,"polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question-id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render (request, "polls/detail.html", {"question" question})
+    question = get_object_or_404(question=question_id)
+    context =  {"question": question}
+    return render (request, "polls/detail.html", context)
 
 def results(request, question_id):
     response = "You are looking at %s."
-    return HttpResponse (response % question_id)
+    return render (request,context)
 
 def vote(request, question_id):
     return HttpResponse ("You are voting on question %s at %s." % question_id)
