811bf2d883977ea196f293217b1f42052d3e70f5
thennys <thennyswhyte@gmail.com>
2025-01-01 12:41:52 -1200

    Casting vote logics implemented

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .models import Choice, Question
 
-from .models import Question
 
 # Create your views here.
 def index(request):
@@ -16,9 +19,25 @@ def detail(request, question_id):
     return render (request, "polls/detail.html", context)
 
 def results(request, question_id):
-    response = "You are looking at %s."
-    return render (request,context)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render (request,"polls/results.html",context)
 
 def vote(request, question_id):
-    return HttpResponse ("You are voting on question %s at %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+                # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
