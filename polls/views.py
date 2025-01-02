f010118a06db48f071b8828b27af3d1da4552e66
thennys <thennyswhyte@gmail.com>
2025-01-01 14:48:11 -1200

    templates updated

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ def index(request):
     return render(request,"polls/index.html", context)
 
 def detail(request, question_id):
-    question = get_object_or_404(question=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context =  {"question": question}
     return render (request, "polls/detail.html", context)
 
